df98ccdef236aa53b5a751cfc656e7088d9d2320
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-05-16 22:34:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if(string === Hello.toLowerCase()) {
+  if(string === string.toLowerCase()) {
   return "I can\'t hear you!!" }
 }
 
 function sayHiToGrandma(string) {
-  if(string === Hello.toUpperCase()) {
+  if(string === string.toUpperCase()) {
   return "YES INDEED!" }
 }
 
