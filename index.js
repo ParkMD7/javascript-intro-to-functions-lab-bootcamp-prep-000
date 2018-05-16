0110ab4d7cd91d6a3d04004585a26c29e6fe5a77
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-05-16 21:03:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function whisper(string) {
 
 function logShout(string) {
   console.log('Hello')
-  return 'Hello'.toUpperCase(spy)
+  return 'Hello'.toUpperCase()
 }
 
 function logWhisper(string) {
